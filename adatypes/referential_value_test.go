87d998cf0dee6a4d9906f9877e11cc59b84a632c
Thorsten A. Knieling <Thorsten.Knieling@softwareag.com>
2019-01-14 23:53:26 +0100

    Adapt tests for referential and super descriptor

---
+++ adatypes/referential_value_test.go
@@ -0,0 +1,39 @@
+package adatypes
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReferential(t *testing.T) {
+	superType := NewReferentialType("AA", 11, [2]string{"AA", "BB"}, 1, 1, 1)
+
+	v, err := superType.Value()
+	assert.NoError(t, err)
+	assert.Equal(t, "AA=REFINT(AA,1,AA)", v.String())
+	sv := v.(*referentialValue)
+	option := &BufferOption{}
+	var buffer bytes.Buffer
+	sv.FormatBuffer(&buffer, option)
+	assert.Equal(t, "", buffer.String())
+	helper := NewHelper([]byte{0x1, 0x2, 0x3, 0x4, 0xff}, 100, endian())
+	sv.parseBuffer(helper, option)
+	assert.Equal(t, []byte{0x1, 0x2, 0x3, 0x4, 0xff}, helper.Buffer())
+	assert.Equal(t, []byte(nil), sv.Bytes())
+	err = sv.SetValue("123")
+	assert.Error(t, err)
+	assert.Equal(t, byte(' '), sv.ByteValue())
+	assert.Equal(t, uint32(0), helper.Offset())
+	sv.StoreBuffer(helper)
+	assert.Equal(t, uint32(0), helper.Offset())
+	_, err = sv.Int32()
+	assert.Error(t, err)
+	_, err = sv.Int64()
+	assert.Error(t, err)
+	_, err = sv.UInt32()
+	assert.Error(t, err)
+	_, err = sv.UInt64()
+	assert.Error(t, err)
+}
---
+++ adatypes/superdesc_value_test.go
@@ -0,0 +1,42 @@
+package adatypes
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSuperDesc(t *testing.T) {
+	opt := byte(0x1)
+	superType := NewSuperType("AA", opt)
+	superType.AddSubEntry("AX", 1, 3)
+	superType.AddSubEntry("AZ", 1, 2)
+	superType.FdtFormat = 'A'
+
+	v, err := superType.Value()
+	assert.NoError(t, err)
+	assert.Equal(t, "", v.String())
+	sv := v.(*superDescValue)
+	option := &BufferOption{}
+	var buffer bytes.Buffer
+	sv.FormatBuffer(&buffer, option)
+	assert.Equal(t, "AA,5,A", buffer.String())
+	helper := NewHelper([]byte{0x1, 0x2, 0x3, 0x4, 0xff}, 100, endian())
+	sv.parseBuffer(helper, option)
+	assert.Equal(t, []byte{0x1, 0x2, 0x3, 0x4, 0xff}, helper.Buffer())
+	assert.Equal(t, []byte{0x1, 0x2, 0x3, 0x4, 0xff}, sv.Bytes())
+	assert.Nil(t, sv.SetValue("123"))
+	assert.Equal(t, byte(' '), sv.ByteValue())
+	assert.Equal(t, uint32(5), helper.Offset())
+	sv.StoreBuffer(helper)
+	assert.Equal(t, uint32(5), helper.Offset())
+	_, err = sv.Int32()
+	assert.Error(t, err)
+	_, err = sv.Int64()
+	assert.Error(t, err)
+	_, err = sv.UInt32()
+	assert.Error(t, err)
+	_, err = sv.UInt64()
+	assert.Error(t, err)
+}
