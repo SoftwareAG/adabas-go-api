a679da4210b5691cd959a705366b6acbdc734ecb
Thorsten A. Knieling <thorsten.knieling@softwareag.com>
2019-03-04 15:50:51 +0100

    Add secure tests

--- adabas/connection_secure_test.go
+++ adabas/connection_secure_test.go
@@ -2,27 +2,55 @@ package adabas
 
 import (
 	"fmt"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-func ExampleConnection_secureRead() {
-	f, err := initLogWithFile("connection_secure.log")
-	if err != nil {
-		fmt.Println(err)
-		return
+func TestConnectionSecure_fail(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping malloc count in short mode")
 	}
+	f := initTestLogWithFile(t, "connection_descriptor.log")
 	defer f.Close()
 
-	connection, cerr := NewConnection("acj;target=25")
-	if cerr != nil {
+	log.Infof("TEST: %s", t.Name())
+	connection, err := NewConnection("acj;target=25;auth=DESC,user=TCMapPoin,id=4,host=UNKNOWN")
+	if !assert.NoError(t, err) {
 		return
 	}
-	defer connection.Close()
+
 	request, rerr := connection.CreateFileReadRequest(11)
-	if rerr != nil {
+	if !assert.NoError(t, rerr) {
 		fmt.Println("Error create request", rerr)
 		return
 	}
 	err = request.QueryFields("AA")
+	assert.Error(t, err)
+	assert.Equal(t, "ADAGEC801F: Security violation: Authentication error (rsp=200,subrsp=31,dbid=25,file=0)", err.Error())
+}
+
+func TestConnectionSecure_pwd(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping malloc count in short mode")
+	}
+	f := initTestLogWithFile(t, "connection_descriptor.log")
+	defer f.Close()
+
+	log.Infof("TEST: %s", t.Name())
+	connection, err := NewConnection("acj;target=25;auth=DESC,user=TCMapPoin,id=4,host=UNKNOWN")
+	if !assert.NoError(t, err) {
+		return
+	}
+	connection.AddCredential("sag", "pwd")
+
+	request, rerr := connection.CreateFileReadRequest(11)
+	if !assert.NoError(t, rerr) {
+		fmt.Println("Error create request", rerr)
+		return
+	}
+	err = request.QueryFields("AA")
+	assert.NoError(t, err)
 	if err != nil {
 		fmt.Println("Error query fields for request", err)
 		return
