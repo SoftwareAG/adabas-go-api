f7d19503ce91229dadda2e564746b931318fd9b0
Thorsten A. Knieling <thorsten.knieling@softwareag.com>
2019-04-11 12:32:58 +0200

    Remove output

--- adabas/map.go
+++ adabas/map.go
@@ -285,11 +285,10 @@ func traverseAdaptType(adaType adatypes.IAdaType, parentType adatypes.IAdaType, 
 			adaType.SetLength(uint32(f.Length))
 			adatypes.Central.Log.Debugf("Check content type=%s", f.ContentType)
 			ct := strings.Split(f.ContentType, ",")
-			fmt.Println("ContentType entry", ct)
 			for _, c := range ct {
 				p := strings.Split(c, "=")
 				if len(p) > 1 {
-					fmt.Println(p[0], "=", p[1])
+					adatypes.Central.Log.Debugf("%s=%s", p[0], p[1])
 					switch p[0] {
 					case "fractionalShift":
 						fs, ferr := strconv.Atoi(p[1])
