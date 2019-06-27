571bd0b4f48065e0ff607e00aceda85231ae30cc
Thorsten A. Knieling <thorsten.knieling@softwareag.com>
2019-06-27 15:00:09 +0200

    Move sh and batch files to script directory

--- scripts/test.bat
+++ scripts/test.bat
@@ -1,7 +1,12 @@
 @echo off
 
+set DIR=%~dp0\..
+
 set CGO_CFLAGS=-I%ACLDIR%\..\inc 
 set CGO_LDFLAGS=-L%ACLDIR% -L%ACLDIR%\..\lib -ladalnkx  
+
+set TESTFILES=%DIR%\files
+set LOGPATH=%DIR%\logs
 
 mkdir test
 go test -timeout 100s -count 1 -tags adalnk -v  github.com/SoftwareAG/adabas-go-api/adabas github.com/SoftwareAG/adabas-go-api/adatypes >test.output
