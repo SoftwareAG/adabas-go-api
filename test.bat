a8d986f6348b7a40f2ee71d5a36fa6873830c1ee
Thorsten A. Knieling <thorsten.knieling@softwareag.com>
2019-06-27 13:28:06 +0200

    Only count 1 test on batch, needed to start tests each time

--- test.bat
+++ test.bat
@@ -4,6 +4,6 @@ set CGO_CFLAGS=-I%ACLDIR%\..\inc
 set CGO_LDFLAGS=-L%ACLDIR% -L%ACLDIR%\..\lib -ladalnkx  
 
 mkdir test
-go test -timeout 100s -tags adalnk -v  github.com/SoftwareAG/adabas-go-api/adabas github.com/SoftwareAG/adabas-go-api/adatypes >test.output
+go test -timeout 100s -count 1 -tags adalnk -v  github.com/SoftwareAG/adabas-go-api/adabas github.com/SoftwareAG/adabas-go-api/adatypes >test.output
 
 %GOPATH%\bin\go2xunit -input test.output -output test\tests.xml
