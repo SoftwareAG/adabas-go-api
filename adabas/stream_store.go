75466db01712847779dfc4449045b4c0f4d761c2
Thorsten A. Knieling <Thorsten.Knieling@softwareag.com>
2021-07-26 18:08:28 +0200

    Fix gofmt error

--- adabas/stream_store.go
+++ adabas/stream_store.go
@@ -1,10 +1,29 @@
+/*
+* Copyright © 2021 Software AG, Darmstadt, Germany and/or its licensors
+*
+* SPDX-License-Identifier: Apache-2.0
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*
+ */
+
 package adabas
 
 import (
 	"github.com/SoftwareAG/adabas-go-api/adatypes"
 )
 
-// ReadLOBRecord read lob records in an stream, repeated call will read next segment of LOB
+// UpdateLOBRecord update lob records in an stream, call will write segment to offset of LOB
 func (request *StoreRequest) UpdateLOBRecord(isn adatypes.Isn, field string, offset uint64, data []byte) (err error) {
 	if adatypes.Central.IsDebugLevel() {
 		adatypes.Central.Log.Debugf("Store LOB record initiated ...")
