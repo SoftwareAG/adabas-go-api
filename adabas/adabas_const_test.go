e02a63e1fc874f3f1acbd6f2bc0144bc44742169
Thorsten A. Knieling <Thorsten.Knieling@softwareag.com>
2021-11-02 11:52:11 +0100

    Correct test check

--- adabas/adabas_const_test.go
+++ adabas/adabas_const_test.go
@@ -11,8 +11,8 @@ func TestCommandCode(t *testing.T) {
 	assert.Equal(t, "S4", cc.command())
 	cc = v3
 	assert.Equal(t, "V3", cc.command())
-	assert.Equal(t, 'V', cc.code()[0])
-	assert.Equal(t, '3', cc.code()[1])
+	assert.Equal(t, byte('V'), cc.code()[0])
+	assert.Equal(t, byte('3'), cc.code()[1])
 
 	assert.True(t, validAcbxCommand(cc.code()))
 	assert.False(t, validAcbxCommand([2]byte{'X', '1'}))
